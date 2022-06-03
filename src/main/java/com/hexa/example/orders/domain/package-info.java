41c582452057b769d2c6618ca6c8f42f1c75b8e3
Othmane MANIAR <othmane_maniar@carrefour.com>
2022-06-03 14:52:15 +0200

    Initial Commit

--- src/main/java/com/hexa/example/orders/domain/package-info.java
+++ src/main/java/com/hexa/example/orders/domain/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * This module is order domain use cases impact
+ * <p>
+ * This module offer orders basic usecases
+ * </p>
+ *
+ * @since 1.0
+ * @author othmane
+ * @version 1.1
+ */
 package com.hexa.example.orders.domain;
--- src/main/java/com/hexa/example/orders/infra/package-info.java
+++ src/main/java/com/hexa/example/orders/infra/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * This module is order infrastructure adapters
+ * <p>
+ * This module offer orders web db and config file adapters
+ * </p>
+ *
+ * @since 1.0
+ * @author othmane
+ * @version 1.1
+ */
 package com.hexa.example.orders.infra;
